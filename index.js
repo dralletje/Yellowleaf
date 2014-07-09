466a9b54b1c280beaeebf8db5d3350a390bc4aad
Dral <m.c.dral@gmail.com>
2014-07-09 11:18:21 +0200

    Last tiny fixes ^^

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-module.exports =
-  ftp: require('./build/ftp')
-  rest: require('./build/sleep')
+module.exports = {
+  ftp: require('./build/ftp'),
+  rest: require('./build/sleep'),
   Drive: require('./build/filesystem')
+}
